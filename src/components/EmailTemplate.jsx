15430cdf740c7624cfc65e76d3610b0be4f0622a
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2024-03-22 15:07:39 -0300

    send emails with resend

---
+++ src/components/EmailTemplate.jsx
@@ -0,0 +1,13 @@
+export const EmailTemplate = ({ firstName }) => {
+  return (
+    <div className="bg-slate-500">
+      <h1 className="text-4xl font-bold">Welcome, {firstName}!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, at.
+      </p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <a href="https://www.google.com">Click me</a>
+      </button>
+    </div>
+  );
+};
